cae33c34844e5d7a504912035587a79e72956e00
Ivan <72845295+ivantheivan5@users.noreply.github.com>
2022-10-16 00:40:04 -0700

    Weather CSV Access Script
    Uses Selenium in Python to access and download a CSV file containing the current day's weather forecast.

---
+++ weather.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("https://www.visualcrossing.com/weather/weather-data-services")
+
+# accept cookies
+driver.find_element(By.XPATH, "//button[@class='btn btn-sm btn-primary col-lg-3']").click()
+
+# search bar
+driver.find_element(By.XPATH, "//input[@class='form-control border-end-0 border-primary']").send_keys("Davis")
